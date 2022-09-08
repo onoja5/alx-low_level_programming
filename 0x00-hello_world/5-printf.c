5bbca1eb5939e9ee0cf7bedb4022198210547df7
onoja5 <onoja16@gmail.com>
2022-09-08 23:03:57 +0000

    updated file added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - with proper grammar, but the outcome is a piece of art
+ *
+ * Main: Entry point
+ * The program prints with proper grammar, but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle,\n");
+			return (0);
+}

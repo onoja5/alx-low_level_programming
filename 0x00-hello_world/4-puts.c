68115cab9d5d684b209fe42d78b411db947d615f
onoja5 <onoja16@gmail.com>
2022-09-08 22:52:47 +0000

    updated script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include <stdio.>
+#include <stdio.h>
+
 /**
- * main - main block
- * 
+ * main - Programming is like building a multilingual puzzle
+ *
+ * Description: main function
+ * The program prints the string in the function
  * Return: 0
- */
+  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }

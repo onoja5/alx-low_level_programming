ed9f98ea7be7ffa9bd73bc77bc3a342467a2514e
onoja5 <onoja16@gmail.com>
2022-09-08 22:38:06 +0000

    updated file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
-/* Program to print Programming is like building a multilingual puzzle */
-
+#include <stdio.>
+/**
+ * main - main block
+ * 
+ * Return: 0
+ */
 int main(void)
-
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

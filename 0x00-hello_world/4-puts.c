d579291f73c10e65100271d0324665abc74e40ba
onoja5 <onoja16@gmail.com>
2022-09-08 20:59:48 +0000

    Updated file added to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *  * main - Entry block
- *   *
- *    * Return - 0
- *     */
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");

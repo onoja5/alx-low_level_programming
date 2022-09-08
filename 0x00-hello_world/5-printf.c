0568b08ee83078b1ee4e4df5355514fa5fb00567
onoja5 <onoja16@gmail.com>
2022-09-08 23:11:54 +0000

    updated script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - with proper grammar, but the outcome is a piece of art
+ * main - print the string in the put function
  *
- * Main: Entry point
- * The program prints with proper grammar, but the outcome is a piece of art
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
- */
+  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }

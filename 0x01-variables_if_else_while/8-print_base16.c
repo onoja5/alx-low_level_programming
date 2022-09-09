a04f5fe07938f0a4137772eba2d541f160a8db61
onoja5 <onoja16@gmail.com>
2022-09-09 22:25:41 +0000

    the script print all numbers of base64 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - this prints all numbers of base 64 in lowercase
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

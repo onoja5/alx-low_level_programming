a277e85c15f3ff673dba9f9d75f35d555a4d7358
onoja5 <onoja16@gmail.com>
2022-09-09 21:54:07 +0000

    Script to print number added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints number number of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}

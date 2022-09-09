2dd0e3962bf5dc282af6b3d57a51f90ef1969209
onoja5 <onoja16@gmail.com>
2022-09-09 22:03:41 +0000

    the script prints numbers of base 10, starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10 starting 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}

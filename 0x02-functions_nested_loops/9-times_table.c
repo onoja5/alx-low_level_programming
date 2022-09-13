36fb160aa0d4a40a39eafd7f6606679c9a09a155
onoja5 <onoja16@gmail.com>
2022-09-13 18:09:03 +0000

    Hot fix added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,39 +6,27 @@
  */
 void time_table(void)
 {
-	int x = 0;
-	int y;
-	int z;
+	int a, b, op;
 
-	while (x < 10)
+	for (a = 0; a <= 9; a++)
 	{
-		y = 0;
-		while (y < 10)
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
 		{
-			z = x * y;
-
-			if (z > 9)
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
 			{
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(z + '0');
+				_putchar(32);
+				_putchar(op + 48);
 			}
 			else
 			{
-				_putchar(z + '0');
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
 			}
-			if (y != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			y++;
 		}
 		_putchar('\n');
-		x++;
 	}
 }

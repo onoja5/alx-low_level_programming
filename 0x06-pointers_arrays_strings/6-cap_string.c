fc1b01d7ff464810a4678c4b9aeaac1d9c1a8f71
onoja5 <onoja16@gmail.com>
2022-09-21 22:57:41 +0000

    Hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,16 +11,16 @@ char *cap_string(char *X)
 	int len = 13;
 	int a = 0, i;
 
-	while (x[a])
+	while (X[a])
 	{
 		i = 0;
 		while (i < len)
 		{
 			if ((a == 0 || X[a - 1] == spc[i] && (X[a] >= 97 && X[a] <= 122))
 					X[a] = X[a] - 32;
-					i++;
-					}
-					a++;
-					}
-					return (X);
-					}
+	i++;
+	}
+	a++;
+	}
+	return (X);
+	}

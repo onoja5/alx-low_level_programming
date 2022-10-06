04825323d04d46eab84f478a9ce3812f3e975198
onoja5 <onoja16@gmail.com>
2022-10-06 22:28:22 +0000

    Hot fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
@@ -26,6 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n > ls2)
 		n = ls2;
+
 	lsout = ls1 + n;
 
 	sout = malloc(lsout + 1);

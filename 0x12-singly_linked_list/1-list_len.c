7a37b506ead1f7b8c0f9fcf74410327f07d791b6
onoja5 <onoja16@gmail.com>
2022-10-20 19:12:50 +0000

    Hot fix

--- 0x12-singly_linked_list/1-list_len.c
+++ 0x12-singly_linked_list/1-list_len.c
@@ -7,13 +7,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
+	size_t number_of_nodes;
 
-	nelem = 0;
+	number_of_nodes = 0;
 	while (h != NULL)
 	{
 		h = h->next;
-		nelem++;
+		number_of_nodes++;
 	}
-	return (nelem);
+	return (number_of_nodes);
 }
